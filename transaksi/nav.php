7acea1990e4a108626bd0e72eaecf36d2e458daa
Penguin Kampus <38419003+penguinkampus@users.noreply.github.com>
2018-07-18 08:58:08 +0700

    Create nav.php

---
+++ transaksi/nav.php
@@ -0,0 +1,79 @@
+<div class="navbar-default sidebar" role="navigation">
+  <div class="sidebar-nav navbar-collapse">
+    <ul class="nav" id="side-menu">
+
+      <li>
+        <a href="../pages/index.php"><i class="glyphicon glyphicon-tasks"></i> DASHBOARD</a>
+      </li>
+      <li>
+        <a href="#"><i class="glyphicon glyphicon-inbox"></i> MASTER<span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li class='nav-item'>
+            <a class='nav-link' href='../master/pasien.php'><i class='fa fa-edit'></i> Entry Data Pasien</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../master/dokter.php'><i class='fa fa-edit'></i> Entry Data Dokter</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../master/obat.php'><i class='fa fa-edit'></i> Entry Data Obat</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../master/tindakan.php'><i class='fa fa-edit'></i> Entry Data Tindakan</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../master/petugas.php'><i class='fa fa-edit'></i> Entry Data Petugas</a>
+          </li>
+        </ul>
+        <!-- /.nav-second-level -->
+      </li>
+      <li>
+        <a href="#"><i class="glyphicon glyphicon-inbox"></i> TRANSAKSI<span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li class='nav-item'>
+            <a class='nav-link' href='daftar.php'><i class='fa fa-edit'></i> Entry Pendaftaran</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='rekmedis.php'><i class='fa fa-edit'></i> Entry Rekam Medis</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='kwitansi.php'><i class='fa fa-edit'></i> Cetak Resep</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='spk.php'><i class='fa fa-edit'></i> Cetak Surat Rujukan</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='stk.php'><i class='fa fa-edit'></i> Cetak Surat Sakit</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='bukden.php'><i class='fa fa-edit'></i> Cetak Kwitansi</a>
+          </li>
+        </ul>
+        <!-- /.nav-second-level -->
+      </li>
+      <li>
+        <a href="#"><i class="glyphicon glyphicon-inbox"></i> LAPORAN<span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/lapsewa.php'><i class='fa fa-edit'></i> Cetak Lap. Pendaftaran</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/lapkembali.php'><i class='fa fa-edit'></i> Cetak Lap. Rekam Medis</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/lapdenda.php'><i class='fa fa-edit'></i> Cetak Lap. Surat Rujukan</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/lapsupir.php'><i class='fa fa-edit'></i> Cetak Lap. Pendapatan</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='../laporan/laprekap.php'><i class='fa fa-edit'></i> Laporan Rekapitulasi Obat</a>
+          </li>
+        </ul>
+        <!-- /.nav-second-level -->
+      </li>
+    </ul>
+  </div>
+  <!-- /.sidebar-collapse -->
+</div>
+<!-- /.navbar-static-side -->
+</nav>
