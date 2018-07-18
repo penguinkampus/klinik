447c893276dc912b63ec448086fd557014fccee0
Penguin Kampus <38419003+penguinkampus@users.noreply.github.com>
2018-07-18 21:24:41 +0700

    Done rekmedis.php

--- transaksi/rekmedis.php
+++ transaksi/rekmedis.php
@@ -2,78 +2,80 @@
 session_start();
 if (isset($_SESSION['login_user'])) {
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
+    <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-  <title>Transaksi Invoice - Artha Laras</title>
-  <!-- Bootstrap Core CSS -->
-  <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-  <link href="../dist/css/bootstrap-toggle.min.css" rel="stylesheet">
-  <link href="../dist/css/github.min.css" rel="stylesheet">
-  <link href="../vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
-  <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-  <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
-  <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-  <link href="../vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
-  <link href="../vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
-  <link href="../vendor/bootstrap-toggle/doc/stylesheet.css" rel="stylesheet">
+        <title>Transaksi Rekam Medis</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+        <link href="../dist/css/bootstrap-toggle.min.css" rel="stylesheet">
+        <link href="../dist/css/github.min.css" rel="stylesheet">
+        <link href="../vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+        <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
+        <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="../vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+        <link href="../vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+        <link href="../vendor/bootstrap-toggle/doc/stylesheet.css" rel="stylesheet">
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
-</head>
-<body>
+    </head>
 
-  <div id="wrapper">
+    <body>
 
-    <?php include 'header.php'; ?>
+        <div id="wrapper">
 
-    <?php include 'nav.php'; ?>
+            <?php include 'header.php'; ?>
 
-    <?php include '../view/rekmedis.php'; ?>
-</div>
-<!-- /#wrapper -->
+                <?php include 'nav.php'; ?>
 
-  <script src="../vendor/jquery/jquery.min.js"></script>
-  <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-  <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-  <script src="../vendor/raphael/raphael.min.js"></script>
-  <script src="../vendor/morrisjs/morris.min.js"></script>
-  <script src="../vendor/bootstrap-toggle/doc/script.js"></script>
-  <script src="../dist/js/morris-data.js"></script>
-  <script src="../dist/js/bootstrap-toggle.min.js"></script>
-  <script src="../dist/js/highlight.min.js"></script>
+                    <?php include '../view/rekmedis.php'; ?>
+        </div>
+        <!-- /#wrapper -->
 
-  <!-- Custom Theme JavaScript -->
-  <script src="../dist/js/sb-admin-2.js"></script>
+        <script src="../vendor/jquery/jquery.min.js"></script>
+        <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+        <script src="../vendor/raphael/raphael.min.js"></script>
+        <script src="../vendor/morrisjs/morris.min.js"></script>
+        <script src="../vendor/bootstrap-toggle/doc/script.js"></script>
+        <script src="../dist/js/morris-data.js"></script>
+        <script src="../dist/js/bootstrap-toggle.min.js"></script>
+        <script src="../dist/js/highlight.min.js"></script>
 
-  <!-- DataTables JavaScript -->
-  <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
-  <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
-  <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+        <!-- Custom Theme JavaScript -->
+        <script src="../dist/js/sb-admin-2.js"></script>
 
-  <script>
-  $(document).ready(function() {
-    $('#dataTables-example').DataTable({
-      responsive: true
-    });
-  });
-  </script>
+        <!-- DataTables JavaScript -->
+        <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+        <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+        <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
 
-</body>
-</html>
-<?php
+        <script>
+            $(document).ready(function() {
+                $('#dataTables-example').DataTable({
+                    responsive: true
+                });
+            });
+        </script>
+
+    </body>
+
+    </html>
+    <?php
 } else {
   header("location: ../pages/login.php");
 }
