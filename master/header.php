e3216ebea251704189cca81ab612991088f9db5f
Penguin Kampus <38419003+penguinkampus@users.noreply.github.com>
2018-07-18 08:52:35 +0700

    Update header.php

--- master/header.php
+++ master/header.php
@@ -7,7 +7,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="../pages/index.php">Artha Laras Rental Mobil</a>
+        <a class="navbar-brand" href="../pages/index.php">Klinik</a>
     </div>
     <!-- /.navbar-header -->
 
