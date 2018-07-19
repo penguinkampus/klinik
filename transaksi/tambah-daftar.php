f0287edb9e29a444986684040cb2502ad31efd43
Penguin Kampus <38419003+penguinkampus@users.noreply.github.com>
2018-07-19 14:31:03 +0700

    Update tambah-daftar.php

--- transaksi/tambah-daftar.php
+++ transaksi/tambah-daftar.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['login_user'])) {
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Transaksi Tambah SPSK - Artha Laras</title>
+  <title>Transaksi Tambah Pendaftaran - Klinik</title>
   <!-- Bootstrap Core CSS -->
   <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
   <link href="../dist/css/bootstrap-toggle.min.css" rel="stylesheet">
