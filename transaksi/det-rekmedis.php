351135129c4c080593a867b0b92404974b22db7e
Penguin Kampus <38419003+penguinkampus@users.noreply.github.com>
2018-07-19 14:28:40 +0700

    Update det-rekmedis.php

--- transaksi/det-rekmedis.php
+++ transaksi/det-rekmedis.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['login_user'])) {
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Detail Rekam Medis</title>
+        <title>Detail Rekam Medis - Klinik</title>
         <!-- Bootstrap Core CSS -->
         <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
         <link href="../dist/css/bootstrap-toggle.min.css" rel="stylesheet">
