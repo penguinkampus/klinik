1e8c4c3302a2b9b377ed91e89d1dba5bcfb580a7
Penguin Kampus <38419003+penguinkampus@users.noreply.github.com>
2018-07-22 21:20:44 +0700

    Update det-kwitansi.php
    saran perubahan format detail kwitansi

--- view/det-kwitansi.php
+++ view/det-kwitansi.php
@@ -1,11 +1,53 @@
 <?php include '../koneksi.php'; ?>
 
+ <?php
+ $nokwitansi	= mysql_real_escape_string($_GET['nokwitansi']);
+
+ $query = mysql_query("
+ 	SELECT * FROM trkwitansi a JOIN trmedis b ON a.nomedis = b.nomedis
+					JOIN trdaftar c ON b.nodaftar = c.nodaftar
+					JOIN dbpasien d ON c.kdpasien = d.kdpasien
+					JOIN detail_obat e ON b.nomedis = e.nomedis
+					JOIN detail_tindakan f ON b.nomedis = f.nomedis 
+					WHERE nokwitansi='$nokwitansi'
+
+
+
+ 	");
+ $data = mysql_fetch_array($query);
+?> 
+
 <div id="page-wrapper">
 <div class="row">
 	<div class="col-lg-12">
 		<h3><span class="glyphicon glyphicon-list-alt"></span>  Detail Kwitansi</h3>
 		<a class="btn" href="kwitansi.php"><span class="glyphicon glyphicon-arrow-left"></span>  Kembali</a>
 
+	<table class="table">
+	<tr>
+			<th class="col-md-3">No. Kwitansi</th>
+			<td><?php echo $data['nokwitansi'] ?></td>
+		</tr>
+		<tr>
+			<th class="col-md-3">Tanggal Kwitansi</th>
+			<td><?php echo $data['tglkwitansi'] ?></td>
+		</tr>
+		<tr>
+			<th class="col-md-3">Nama Pasien</th>
+			<td><?php echo $data['nmpasien'] ?></td>
+		</tr>
+
+</table>
+<hr>
+	<table class="table">
+<tr>
+
+			<th class="col-md-2">Obat</th>
+			<th class="col-md-2">Total Harga Obat</th>
+			<th class="col-md-2">Tindakan</th>
+			<th class="col-md-2">Total Harga Tindakan</th>
+			<th class="col-md-3">Sub Total</th>
+		</tr>
 <?php
 $nokwitansi	= mysql_real_escape_string($_GET['nokwitansi']);
 $det = mysql_query("SELECT * FROM trkwitansi a JOIN trmedis b ON a.nomedis = b.nomedis
@@ -16,35 +58,27 @@ $det = mysql_query("SELECT * FROM trkwitansi a JOIN trmedis b ON a.nomedis = b.n
 					WHERE nokwitansi='$nokwitansi'
 					")or die(mysql_error());
 while($d=mysql_fetch_array($det)){
+	
 	?>
-	<table class="table">
+
 		<tr>
-			<th class="col-md-3">No. Kwitansi</th>
-			<td><?php echo $d['nokwitansi'] ?></td>
-		</tr>
-		<tr>
-			<th class="col-md-3">Tanggal Kwitansi</th>
-			<td><?php echo $d['tglkwitansi'] ?></td>
-		</tr>
-		<tr>
-			<th class="col-md-3">Nama Pasien</th>
-			<td><?php echo $d['nmpasien'] ?></td>
-		</tr>
-		<tr>
-			<th class="col-md-3">Pembayaran Obat</th>
 			<td><?php echo $d['nmobat'] ?></td>
-		</tr>
-		<tr>
-			<th class="col-md-3">Pembayaran Tindakan</th>
+			<td>Rp. <?php echo $d['totalobat'] ?></td>
 			<td><?php echo $d['nmtindakan'] ?></td>
+			<td>Rp. <?php echo $d['totaltindakan'] ?></td>
+			<td><b>Rp. <?php echo $data['subtotal'] ?></b></td>
 		</tr>
-		<tr>
-			<th class="col-md-3">Total Harga</th>
-			<td>Rp. <?php echo $d['subtotal'] ?></td>
-		</tr>
-	</table>
+	
+	
 	<?php
 }
 ?>
+<tr>	
+			<td></td>
+			<td></td>
+			
+			<td></td>
+		</tr>
+</table>
 </div>
 </div>
