521cae549f54ce1b5e3618060a7d2c35138ff0e9
Penguin Kampus <38419003+penguinkampus@users.noreply.github.com>
2018-07-23 22:10:02 +0700

    Add files via upload
    Last But not Least Laporan

---
+++ view/laprekapmedis.php
@@ -0,0 +1,60 @@
+<?php
+include '../koneksi.php';
+
+if (isset($_POST['submit'])) {
+  $tgl_awal   = $_POST['tgl_awal'];
+  // $tgl_akhir  = $_POST['tgl_akhir'];
+
+  $simpan = mysql_query("
+      SELECT * from trdaftar a
+      -- JOIN dbpasien b ON a.kdpasien = b.kdpasien
+      WHERE a.tgldaftar <= '$tgl_awal' ");
+  if ($simpan) {
+    echo "<script>alert('Laporan Penyewaan Siap di Cetak!');window.location='cetaklaprekapmedis.php?tgl_awal=$tgl_awal';</script>";
+  } else {
+    echo "<script>alert('Laporan Penyewaan Gagal di Cetak!');window.location='laprekapmedis.php';</script>";
+  }
+}
+?>
+
+<div id="page-wrapper">
+  <div class='row'>
+    <div class='col-lg-12'>
+      <h3 class="page-header"><span class="glyphicon glyphicon-file"></span> Cetak Laporan Rekapitulasi Medis</h3>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-12">
+      <form action="" method="POST" enctype="multipart/form-data" class="form-horizontal">
+        <div class="form-group row">
+          <label class="col-md-2 form-control-label" for="text-input">Tanggal Periode</label>
+          <div class="col-md-3">
+            <input type="date" id="tgl_awal" name="tgl_awal" class="form-control" required>
+          </div>
+        </div>
+     
+
+        <div class="form-group row">
+          <label class="col-md-2 form-control-label" for="text-input"></label>
+          <div class="col-md-5">
+            <button type="submit" name="submit" class="btn btn-primary">CETAK</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<!-- /#page-wrapper -->
+
+<!--Code JS Validation-->
+<script language='javascript'>
+function validAngka(a)
+{
+  if(!/^[0-9.]+$/.test(a.value))
+  {
+    a.value = a.value.substring(0,a.value.length-1000);
+  }
+}
+</script>
+<!--End of Code JS Validation-->
